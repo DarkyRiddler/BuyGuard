fc8e46c2cd86e7b4c571a5b57239feb00e840dbe
dawid <dawid566123@gmail.com>
2025-07-09 13:40:31 +0200

    usuwanie tokena przy przyszlym wylogowywaniu

--- client/app/set-cookie-test/page.tsx
+++ client/app/set-cookie-test/page.tsx
@@ -2,6 +2,7 @@
 'use client';
 
 import { useState } from 'react';
+import { Button } from '@/components/ui/button';
 
 async function setCookie() {
   console.log('setCookie clicked');
@@ -13,31 +14,45 @@ async function setCookie() {
   const data = await res.json();
   console.log(data);
 }
+
+async function deleteCookie() {
+  const res = await fetch('api/logout', {
+    method: 'POST',
+    credentials: 'include',
+  })
+  if (res.status !== 200) {
+    console.log(res.statusText);
+  }
+}
+
 export default function Page() {
-  
+
   const [message, setMessage] = useState<string>('Brak cookie');
-  
+
   async function getCookie() {
     console.log('getCookie clicked');
-    const res = await fetch('api/get-cookie', {})
+    const res = await fetch('api/get-cookie', {});
     if (res.status === 200) {
       const cookie = await res.json();
-      console.log(cookie)
+      console.log(cookie);
       if (cookie.jwt) {
-        setMessage(cookie.jwt)
+        setMessage(cookie.jwt);
       } else {
         setMessage('Cookie not found');
       }
     }
   }
-  
+
   return (
     <div className="flex flex-col gap-4 items-center justify-center h-screen">
-    <button onClick={setCookie}>
-      Ustaw Cookie
-    </button>
-      <button onClick={getCookie}>Zobacz zawartość cookie</button>
+      <Button onClick={setCookie}>
+        Ustaw Cookie
+      </Button>
+      <Button onClick={getCookie}>Zobacz zawartość cookie</Button>
       <p>{message}</p>
+      <Button onClick={deleteCookie}>
+        Usuń Cookie (Logout)
+      </Button>
     </div>
   );
 }
