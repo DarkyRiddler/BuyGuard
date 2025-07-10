e9a86635d8332c67ae8df1368bbd6144ab32b342
rendor535 <marcinbos2004@gmail.com>
2025-07-10 12:09:30 +0200

    Controller poprawiony
    nie wiem jak porównywać role i skad to info brac

--- server/Models/User.cs
+++ server/Models/User.cs
@@ -8,9 +8,10 @@ public class User
     public int Id { get; set; }
     public string Email { get; set; }
     public string PasswordHash { get; set; }
-    public string Role { get; set; } // "admin", "manager", "client"
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Role { get; set; } // "admin", "manager", "client", ewemtualnie "CEO"
     public decimal ManagerLimitPln { get; set; }
-
     public ICollection<Request> Requests { get; set; }
     public ICollection<Request> ManagedRequests { get; set; }
     public ICollection<Note> Notes { get; set; }
