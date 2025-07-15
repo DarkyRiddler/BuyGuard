30d19daac06b2fbeef63458e7b67d3ad776b0439
dawid <dawid566123@gmail.com>
2025-07-15 20:19:22 +0200

    jakis blad wywala, ale dark mode dziala XD

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -29,12 +29,11 @@ export default function RootLayout({
     <body
       className={`${geistSans.variable} ${geistMono.variable} antialiased`}
     >
-    <ThemeProvider>
-      <Toaster/>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <Toaster />
       {children}
     </ThemeProvider>
     </body>
     </html>
-  )
-    ;
+  );
 }
