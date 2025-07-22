fc8972e4f6c14b77b0e33328f367857a88e21ead
torifair <nikola042@onet.pl>
2025-07-22 21:35:59 +0200

    edit

--- client/types/index.d.ts
+++ client/types/index.d.ts
@@ -7,5 +7,13 @@
   managerLimitPln: number;
   role: Role;
 }
+export interface Request
+{
+  title: string;
+  amount_pln: number;
+  description: string;
+  reason: string;
+  link: string;
+}
 
 export type Role = 'admin' | 'manager' | 'employee';
